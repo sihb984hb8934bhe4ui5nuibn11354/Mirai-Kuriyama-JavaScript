65b6fd2df1ee5d4899c87d858d8440f19af94e0a
23o0r9tvnuriogj43giobutgnevoguv5houj4 <unconfigured@null.spigotmc.org>
2018-06-14 12:57:06 -0300

    asdfasdfasdf

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+var token = process.env.TOKEN;
+
+
+bot.on('message', message => {
+	if (message.content.startsWith("&javascript")){
+		return message.channel.send("<@"+message.author.id+"> -- Quero ter minha parte javascript :angry:");
+	}
+})
+
+bot.login(token);
