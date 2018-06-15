81a8bb84e6421d8701c3a11b7e40c4307370719f
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-06-14 21:53:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
 	if (message.content.startsWith("&javascript")){
-		await message.channel.send("<@"+message.author.id+"> -- Quero ter minha parte javascript :angry:");
+		return message.channel.send("<@"+message.author.id+"> -- Quero ter minha parte javascript :angry:");
 	}
 })
 
