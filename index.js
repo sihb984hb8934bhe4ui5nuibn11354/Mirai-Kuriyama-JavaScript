faa7c0823637dec20ac52249ca7e229a15d92f0c
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-06-14 21:05:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 var token = process.env.TOKEN;
-function sleep(milliseconds) {var start = new Date().getTime();for (var i = 0; i < 1e7; i++) {if ((new Date().getTime() - start) > milliseconds){break;}}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 
 
 bot.on('ready', () => {
