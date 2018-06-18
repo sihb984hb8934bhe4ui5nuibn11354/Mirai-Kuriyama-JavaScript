548f74b4ffd9538e9f2fbbc89c8864b9943f5a8e
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-06-18 17:12:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var token = process.env.TOKEN;
 	
 bot.on('ready', () => {
 	console.log("Javascript Injectato!")
+	bot.user.setGame("amor para " +bot.guids.size+" servidores","https://www.twitch.tv/deivizin_")
 })
 
 bot.on('message', message => {
